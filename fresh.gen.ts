8f2888c8202fe621136f5d4169b8d4042b729ef7
custompro98 <mitchjoa@gmail.com>
2023-08-28 16:06:25 -0400

    rm default fresh components, etc.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,27 +5,20 @@
 import * as $0 from "./routes/[provider]/callback.ts";
 import * as $1 from "./routes/_404.tsx";
 import * as $2 from "./routes/_app.tsx";
-import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/greet/[name].tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/signin.ts";
-import * as $7 from "./routes/signout.ts";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/signin.ts";
+import * as $5 from "./routes/signout.ts";
 
 const manifest = {
   routes: {
     "./routes/[provider]/callback.ts": $0,
     "./routes/_404.tsx": $1,
     "./routes/_app.tsx": $2,
-    "./routes/api/joke.ts": $3,
-    "./routes/greet/[name].tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/signin.ts": $6,
-    "./routes/signout.ts": $7,
+    "./routes/index.tsx": $3,
+    "./routes/signin.ts": $4,
+    "./routes/signout.ts": $5,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 };
 
