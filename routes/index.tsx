c12a8afa01916f862cb94e825e67ac45323a28e3
custompro98 <mitchjoa@gmail.com>
2023-08-28 16:09:18 -0400

    AuthN frontend

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,33 @@
-export default function Home() {
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { getSessionAccessToken, getSessionId } from "deno-kv-oauth";
+import { Profile, googleOauth2Client } from "../utils/oauth2_client.ts";
+
+export const handler: Handlers<Profile | null> = {
+  async GET(req, ctx) {
+    const sessionId = await getSessionId(req);
+
+    if (!sessionId) {
+      return ctx.render(null);
+    }
+
+    const accessToken = await getSessionAccessToken(
+      googleOauth2Client,
+      sessionId
+    );
+    const response = await fetch(
+      "https://openidconnect.googleapis.com/v1/userinfo",
+      {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    const user: Profile = await response.json();
+    return ctx.render(user);
+  },
+};
+
+export default function Home({ data }: PageProps<Profile>) {
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -10,6 +39,15 @@ export default function Home() {
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
         <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        {data ? (
+          <div className="flex flex-col items-center">
+            <span className="py-1">{data.email}</span>
+            <img className="py-1" src={data.picture.toString()} width="64" height="64" />
+            <a href="/signout">Sign out</a>
+          </div>
+        ) : (
+          <a href="/signin">Sign in</a>
+        )}
       </div>
     </div>
   );
